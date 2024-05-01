16181fd679c5668178cf0b2e7e1bcb9c9158fd0c
Lina <knocker767@gmail.com>
2024-05-02 00:43:14 +0200

    feat: poetry instead of setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='pythonQEPest',
-    author="GepardLianna",
-    author_email="knocker767@gmail.com",
-    version='1.0.0',
-    py_modules=['pydantic']
-)
