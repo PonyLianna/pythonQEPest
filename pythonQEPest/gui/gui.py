e9fd26129b54f0ee77307a0774b56a23a5ddf1e1
Lina <knocker767@gmail.com>
2025-07-14 01:13:52 +0200

    feat[GUI]: click_right, click_left and style

--- pythonQEPest/gui/gui.py
+++ pythonQEPest/gui/gui.py
@@ -1,9 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
 
-import tkinter as tk
-from tkinter import filedialog, messagebox, ttk
-
 import pyperclip
 
 from pythonQEPest.core.qepest import QEPest
@@ -28,27 +25,44 @@ class GUI:
 
         frame_buttons.pack(anchor='w', fill='x', pady=5)
 
-        tk.Button(frame_buttons, text="Select File", command=self.load_file).pack(side='left', padx=2)
-        tk.Button(frame_buttons, text="Add entry", command=self.add_entry).pack(side='left', padx=2)
-        tk.Button(frame_buttons, text="Delete entry", command=self.delete_selected).pack(side='left', padx=2)
-        tk.Button(frame_buttons, text="Copy Data", command=self.copy_selected).pack(side='left', padx=2)
-        tk.Button(frame_buttons, text="Paste Data", command=self.paste_entries).pack(side='left', padx=2)
-        tk.Button(frame_buttons, text="Process Data", command=self.process_data).pack(side='right', padx=2)
+        tk.Button(frame_buttons, text="Select File", command=self.load_file).pack(side='left', padx=[10,4])
+        tk.Button(frame_buttons, text="Add entry", command=self.add_entry).pack(side='left', padx=4)
+        tk.Button(frame_buttons, text="Delete entry", command=self.delete_selected).pack(side='left', padx=4)
+        tk.Button(frame_buttons, text="Copy Data", command=self.copy_selected).pack(side='left', padx=4)
+        tk.Button(frame_buttons, text="Paste Data", command=self.paste_entries).pack(side='left', padx=4)
+        tk.Button(frame_buttons, text="Process Data", command=self.process_data).pack(side='right', padx=10)
 
         self.data_tree = ttk.Treeview(self.root, columns=('ID', 'Name', 'MW', 'LogP', 'HBA', 'HBD', 'RB', 'arR'), show='headings')
 
         for col in self.data_tree['columns']:
             self.data_tree.heading(col, text=col)
-        self.data_tree.pack(padx=5, pady=5, fill='both', expand=True)
+        self.data_tree.pack(padx=10, pady=10, fill='both', expand=True)
 
-        self.result_tree = ttk.Treeview(self.root, columns=('Name', 'QEH', 'QEI', 'QEF'), show='headings')
+        self.result_tree = ttk.Treeview(self.root, columns=('ID', 'Name', 'QEH', 'QEI', 'QEF'), show='headings')
 
         for col in self.result_tree['columns']:
             self.result_tree.heading(col, text=col)
-        self.result_tree.pack(padx=2, pady=10, fill='both', expand=True)
+        self.result_tree.pack(padx=10, pady=10, fill='both', expand=True)
 
         self.save_button = tk.Button(self.root, text="Save Results", command=self.save_result, state='disabled')
-        self.save_button.pack(padx=2, pady=10, side='right')
+        self.save_button.pack(padx=10, pady=10, side='right')
+
+        self.data_tree.unbind_class("Treeview", "<Button-1>")
+        self.result_tree.unbind_class("Treeview", "<Button-1>")
+
+        self.data_tree.unbind_class("Treeview", "<Button-2>")
+        self.result_tree.unbind_class("Treeview", "<Button-2>")
+
+        self.data_tree.bind("<Button-1>", self.on_treeview_click_left)
+        self.result_tree.bind("<Button-1>", self.on_treeview_click_left)
+
+        self.menu = tk.Menu(root, tearoff=0)
+        self.menu.add_command(label="Copy", command=self.copy_selected)
+        self.menu.add_command(label="Paste", command=self.paste_entries)
+        self.menu.add_command(label="Delete", command=self.delete_selected)
+
+        self.data_tree.bind("<Button-3>", self.on_treeview_click_right)
+        self.result_tree.bind("<Button-3>", self.on_treeview_click_right)
 
     def copy_selected(self):
         selected = self.data_tree.selection()
@@ -150,9 +164,10 @@ class GUI:
             idx_to_remove = int(values[0])
             self.file_data = [row for row in self.file_data if row[0] != idx_to_remove]
             self.data_tree.delete(item)
+            self.result_tree.delete(item)
 
         messagebox.showinfo("Deleted", "The selected records have been deleted. Please reprocess the data.")
-        self.result_tree.delete(*self.result_tree.get_children())
+        # self.result_tree.delete(*self.result_tree.get_children())
         self.save_button.config(state='disabled')
 
     def process_data(self):
@@ -164,7 +179,7 @@ class GUI:
         self.result_tree.delete(*self.result_tree.get_children())
 
         for row in self.file_data:
-            result_row = self.qepest.compute_params(QEPestInput.from_array(row[1:])).to_array()
+            result_row = [row[0], *self.qepest.compute_params(QEPestInput.from_array(row[1:])).to_array()]
             self.result_data.append(result_row)
             self.result_tree.insert('', 'end', values=result_row)
 
@@ -190,6 +205,27 @@ class GUI:
 
         except Exception as e:
             messagebox.showerror("Saving error", str(e))
+
+    def on_treeview_click_left(self, event):
+        tree = event.widget
+        item_id = tree.identify_row(event.y)
+        if not item_id:
+            return
+        selected = tree.selection()
+        if item_id in selected:
+            tree.selection_remove(item_id)
+        else:
+            tree.selection_set(item_id) # [*selected, item_id]
+
+    def on_treeview_click_right(self, event):
+        tree = event.widget
+        item_id = tree.identify_row(event.y)
+        if item_id:
+            tree.selection_set(item_id)
+            self.menu.tk_popup(event.x_root, event.y_root)
+        else:
+            pass
+
 
 
 if __name__ == '__main__':
