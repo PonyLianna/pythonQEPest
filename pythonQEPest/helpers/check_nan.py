b29b3e6467d7ec9330cd9bdcdaba0801686c40bc
Lina <knocker767@gmail.com>
2025-07-19 22:16:01 +0200

    feat[helpers]: typings fix

--- pythonQEPest/helpers/check_nan.py
+++ pythonQEPest/helpers/check_nan.py
@@ -1,7 +1,7 @@
 import math
 
 
-def check_nan(d) -> int:
+def check_nan(d) -> list[float]:
     for i in range(len(d)):
         if math.isnan(d[i]):
             d[i] = 0.0
--- pythonQEPest/helpers/round_to_4digs.py
+++ pythonQEPest/helpers/round_to_4digs.py
@@ -1,2 +1,2 @@
-def round_to_4digs(q):
+def round_to_4digs(q) -> float:
     return float("{:.4f}".format(q))
