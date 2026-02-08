03f58405ca566ac23d327b8cad6ac7af81e9f2e7
Lina <knocker767@gmail.com>
2026-02-08 18:31:40 +0100

    feat: logger + env

--- pythonQEPest/main.py
+++ pythonQEPest/main.py
@@ -1,5 +1,14 @@
+import os
 from pythonQEPest.cli.cli import CLI
 from pythonQEPest.core.qepest import QEPest
+from pythonQEPest.logger import init_logger
+from dotenv import load_dotenv
+
 
 if __name__ == "__main__":
+    load_dotenv()
+
+    if os.getenv("APP_DEBUG_ENABLE", "false").lower() == "true":
+        init_logger()
+
     cli = CLI(qepest=QEPest()).read_file_and_compute_params()
